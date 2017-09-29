8701b2aaaf5d8b63c382fc641f401b316213d378
Ebon Chung <jay0000015chung@gmail.com>
2017-09-29 14:42:52 -0700

    close#8 Removed the arguments for the function

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,7 +53,7 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['i/(:any).+'] = 'golf';
-$route['bananas/rule'] = function($fruit,$verb) {
+$route['bananas/rule'] = function() {
 return 'golf';
 };
 $route['foxtrot'] = 'tango';
